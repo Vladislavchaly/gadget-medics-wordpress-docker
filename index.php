8f4aef11d3c16df67647488e6f152a1ac31bc15d
chaly95@gmail.com <qsuNRknX2fawkpXLsrVZ>
2022-07-10 17:03:26 +0300

    init project

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
 <?php
-
-//$link = mysql_connect(
-//    "mysql",
-//    "gadgeto0_GMDB",
-//    "Atkqrn8FNZHn",
-//    $new_link = false,
-//    $client_flags = 0
-//);
-//if (!$link) {
-//    die('Ошибка соединения: ' . mysql_error());
-//}
-//mysql_close($link);
-//die('Успешно соединились');
-
-//var_dump(mysqli_connect(
-// ini_get("mysql"),
-//ini_get("gadgeto0_GMDB"),
-//ini_get("Atkqrn8FNZHn"),
-//$new_link = false,
-//$client_flags = 0
-//));
-
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
